cb9d3791842d3eecfaf5ab04b8c2021d12be290d
Dustin Lo <dustinlo.pcc@gmail.com>
2018-04-29 15:07:36 -0700

    using confluent connector (port 8084) image i created

--- commands.sh
+++ commands.sh
@@ -1,14 +1,10 @@
 # run image
 docker-compose -f docker-compose.yml up
-
 # safely take image down
 docker-compose -f docker-compose.yml down
 
 # creates 3 brokers
 docker-compose scale kafka=3
-
-# enters container for only 1 broker
-# docker exec -it kafkadocker_kafka_1 bash
 
 # enters container for multiple brokers
 docker run --rm -it --net=host wurstmeister/kafka bash
@@ -19,6 +15,7 @@ docker run --rm -it --net=host wurstmeister/kafka bash
 # works for multiple brokers
 kafka-topics.sh --list --zookeeper localhost:2181
 
+# add n more brokers (cant get it to work on local)
 docker-compose scale kafka=3
 
 kafka-topics.sh --create --zookeeper 127.0.0.1:2181 --replication-factor 1 --partitions 1 --topic topic1
@@ -28,6 +25,7 @@ kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic topic1 --fro
 
 docker cp register-postgres.json wurstmeister/kafka
 
+# access postgres server
 docker-compose -f docker-compose.yml exec postgres env PGOPTIONS="--search_path=inventory" bash -c 'psql -U postgres postgres'
 
 
@@ -50,17 +48,13 @@ curl -s -X PUT 127.0.0.1:8083/connectors/file-stream-demo-distributed/pause
 curl -s -X PUT 127.0.0.1:8083/connectors/file-stream-demo-distributed/resume
 curl -s 127.0.0.1:8083/connectors/file-stream-demo-distributed | jq
 curl -s -X DELETE 127.0.0.1:8083/connectors/file-stream-demo-distributed
-curl -s -X POST -H "Content-Type: application/json" --data '{"name": "file-stream-demo-distributed", "config":{"connector.class":"org.apache.kafka.connect.file.FileStreamSourceConnector","key.converter.schemas.enable":"true","file":"/demo-file.txt","tasks.max":"1","value.converter.schemas.enable":"true","name":"file-stream-demo-distributed","topic":"demo-2-distributed","value.converter":"org.apache.kafka.connect.json.JsonConverter","key.converter":"org.apache.kafka.connect.json.JsonConverter"}}' http://127.0.0.1:8083/connectors | jq
+curl -s http://127.0.0.1:8084/connectors -X POST -H "Content-Type: application/json" --data '{"name": "file-stream-demo-distributed", "config":{"connector.class":"org.apache.kafka.connect.file.FileStreamSourceConnector","key.converter.schemas.enable":"true","file":"/demo-file.txt","tasks.max":"1","value.converter.schemas.enable":"true","name":"file-stream-demo-distributed","topic":"demo-2-distributed","value.converter":"org.apache.kafka.connect.json.JsonConverter","key.converter":"org.apache.kafka.connect.json.JsonConverter"}}' | jq
 curl -s -X PUT -H "Content-Type: application/json" --data '{"connector.class":"org.apache.kafka.connect.file.FileStreamSourceConnector","key.converter.schemas.enable":"true","file":"demo-file.txt","tasks.max":"2","value.converter.schemas.enable":"true","name":"file-stream-demo-distributed","topic":"demo-2-distributed","value.converter":"org.apache.kafka.connect.json.JsonConverter","key.converter":"org.apache.kafka.connect.json.JsonConverter"}' 127.0.0.1:8083/connectors/file-stream-demo-distributed/config | jq
 
 
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @register-postgres.json
-
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d '{"name":"inventory-connector","config":{"connector.class":"io.debezium.connector.postgresql.PostgresConnector","tasks.max":"1","database.hostname":"postgres","database.port":"5432","database.user":"postgres","database.password":"postgres","database.dbname":"postgres","database.server.name":"dbserver1","database.whitelist":"inventory","database.history.kafka.bootstrap.servers":"kafka:9092","database.history.kafka.topic":"schema-changes.inventory"}}'
 
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8084/connectors/ -d @register-twitter.json
 
 kafka-topics.sh --list --zookeeper localhost:2181
 kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic dbserver1.inventory.orders --from-beginning
-
-
